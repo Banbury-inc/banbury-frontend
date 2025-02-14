9ecffa94d495e99cc90ab1258163aea7f3914b68
Michael Mills <mmills6060@gmail.com>
2025-02-14 14:13:58 -0500

    settings fixed

--- packages/frontend/src/renderer/components/pages/Settings/Settings.tsx
+++ packages/frontend/src/renderer/components/pages/Settings/Settings.tsx
@@ -109,30 +109,6 @@ export default function Settings() {
 
   return (
     <Box sx={{ width: '100%', pt: 0 }}>
-      <Card variant='outlined' sx={{ borderTop: 0, borderLeft: 0, borderBottom: 0 }}>
-        <CardContent sx={{ paddingBottom: '2px !important', paddingTop: '46px' }}>
-          <Stack spacing={2} direction="row" sx={{ flexWrap: 'nowrap' }}>
-            <Grid container spacing={0} sx={{ display: 'flex', flexWrap: 'nowrap', pt: 0 }}>
-
-            </Grid>
-            <Grid container justifyContent='flex-end' alignItems='flex-end'>
-              <Grid item>
-              </Grid>
-              <Grid item>
-
-                <Box sx={{ display: 'flex', justifyContent: 'flex-end', alignItems: 'flex-end' }}>
-                  <Stack direction="row">
-                    <NotificationsButton />
-                  </Stack>
-                  <Stack paddingLeft={1} direction="row">
-                    <AccountMenuIcon />
-                  </Stack>
-                </Box>
-              </Grid>
-            </Grid>
-          </Stack>
-        </CardContent>
-      </Card>
       <Stack direction="row" spacing={0} sx={{ width: '100%', height: 'calc(100vh - 76px)', overflow: 'hidden' }}>
         <Stack 
           sx={{ 
