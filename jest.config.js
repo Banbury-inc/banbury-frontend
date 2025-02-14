89b278507ab63f997c6c3a22f9ce4d125d879af9
Michael Mills <mmills6060@gmail.com>
2025-02-13 22:59:49 -0500

    set verbose to true

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   preset: 'ts-jest',
+  verbose: true,
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules/', '/dist/'],
   moduleNameMapper: {
--- packages/core/src/files/__tests__/newUseFileData.test.ts
+++ packages/core/src/files/__tests__/newUseFileData.test.ts
@@ -13,9 +13,6 @@ jest.mock('react', () => ({
 
 describe('newUseFileData', () => {
     // Mock functions
-    const setFirstname = jest.fn();
-    const setLastname = jest.fn();
-    const setDevices = jest.fn();
 
     beforeEach(() => {
         jest.clearAllMocks();
@@ -27,41 +24,7 @@ describe('newUseFileData', () => {
         jest.restoreAllMocks();
     });
 
-    const defaultProps = {
-        username: 'testUser',
-        disableFetch: false,
-        updates: 0,
-        global_file_path: null,
-        global_file_path_device: null,
-        setFirstname,
-        setLastname,
-        files: [],
-        sync_files: [],
-        devices: [],
-        setDevices,
-    };
 
-    it('should fetch devices when devices prop is null', async () => {
-        const mockDevices = [{ device_name: 'test-device', online: true }];
-        mockFetchDeviceData.mockResolvedValueOnce(mockDevices);
-
-        const propsWithNullDevices = { ...defaultProps, devices: null };
-
-        // Import and call the hook function directly
-        const { newUseFileData } = require('../../files/newUseFileData');
-        newUseFileData(...Object.values(propsWithNullDevices));
-
-        expect(mockFetchDeviceData).toHaveBeenCalledWith(
-            'testUser',
-            false,
-            '',
-            expect.objectContaining({
-                setFirstname,
-                setLastname,
-                setDevices,
-            })
-        );
-    });
 
 
 
