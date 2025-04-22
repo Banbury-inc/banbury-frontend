9a7217a1329b0c5231489526b43793d01704ea12
Michael Mills <mmills6060@gmail.com>
2025-04-22 11:11:54 -0400

    successfully sending transfer request

--- packages/core/src/files/index.ts
+++ packages/core/src/files/index.ts
@@ -1,3 +1,5 @@
+import { cancel_download_request as cancelDownloadRequestInternal } from '../websocket/files/file_transfer';
+
 export * from './addFiles';
 export * from './removeFiles';
 export * from './searchFile';
@@ -9,4 +11,5 @@ export * from './addDeviceIdtoFileSyncFiles';
 export * from './shareFile';
 export * from './makeFilePublic';
 export * from './makeFilePrivate';
-export { cancel_download_request } from '../websocket/files/file_transfer';
+
+export const cancel_download_request = cancelDownloadRequestInternal;
