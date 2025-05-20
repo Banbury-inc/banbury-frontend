f786bcea906dfe11011e7e4d200907bfb3781db8
Michael Mills <mmills6060@gmail.com>
2025-05-20 15:35:41 -0400

    fix build error

--- packages/frontend/src/components/pages/Devices/components/ScannedFoldersChips.tsx
+++ packages/frontend/src/components/pages/Devices/components/ScannedFoldersChips.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Stack, Chip } from '@mui/material';
-import { remove_scanned_folder } from '@banbury/core/dist/device/remove_scanned_folder';
+import { banbury } from "@banbury/core";
 
 interface ScannedFoldersChipsProps {
   scanned_folders: string[];
@@ -8,11 +8,11 @@ interface ScannedFoldersChipsProps {
   onFoldersUpdate: () => void; // New callback prop
 }
 
-export default function ScannedFoldersChips({ scanned_folders, username, onFoldersUpdate }: ScannedFoldersChipsProps) {
+export default function ScannedFoldersChips({ scanned_folders, onFoldersUpdate }: ScannedFoldersChipsProps) {
   // Handler to delete a folder from the list
   const handleDeleteFolder = async (folderToDelete: string) => {
     try {
-      await remove_scanned_folder(folderToDelete, username);
+      await banbury.device.removeScannedFolder(folderToDelete);
       onFoldersUpdate(); // Call the callback to trigger a refresh
     } catch (error) {
       console.error("Error deleting folder:", error);
