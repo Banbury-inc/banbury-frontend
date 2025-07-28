89f249e7d932900054ac603d6b9342dfa2fce138
Michael Mills <mmills6060@gmail.com>
2025-07-28 12:31:29 -0400

    ensure arrow keys and enter key work when navigating mention list

--- packages/frontend/src/components/pages/Workspaces/components/ChipRichTextInput.tsx
+++ packages/frontend/src/components/pages/Workspaces/components/ChipRichTextInput.tsx
@@ -86,7 +86,7 @@ const ChipRichTextInput: React.FC<ChipRichTextInputProps> = ({
                   popup[0].hide();
                   return true;
                 }
-                return (component?.ref as any)?.handleKeyDown?.(props.event) || false;
+                return (component?.ref as any)?.onKeyDown?.({ event: props.event }) || false;
               },
               onExit() {
                 popup?.[0]?.destroy();
